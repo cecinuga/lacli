c1c74bbb96c42367d5bec9d65d8673500b576c27
cecinuga <cecinuga.tdb@gmail.com>
2026-06-25 20:52:38 +0200

    add support for n_thread 11

--- tests/conftest.py
+++ tests/conftest.py
@@ -30,7 +30,7 @@ def csv_reader(dataset_path):
 
 @pytest.fixture(
     scope="package",
-    params=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32]
+    params=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32]
 )
 def n_thread(request):
     """Yield each candidate worker-thread count to exercise `run` under varying parallelism."""
