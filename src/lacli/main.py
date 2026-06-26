3b3c480c89a1d5beb1a4aa84b88d66998913d647
cecinuga <cecinuga.tdb@gmail.com>
2026-06-26 23:29:40 +0200

    refactored

--- src/lacli/main.py
+++ src/lacli/main.py
@@ -5,19 +5,19 @@ Every line is intended to be an array of the matrix. To load the matrix, the fil
 split into byte chunks (one per thread plus a remainder), each chunk is lexed
 concurrently, and the resulting per-chunk token lists are stitched back into a Matrix.
 """
-from argparse import Namespace
 import os
 import sys
-from lacli.load.file import load
+from argparse import Namespace
+import lacli.load.file as file
 from lacli.arg import get_argparse
 from lacli.benchmark.timer import timer
 from lacli.models.matrix import Matrix
 
-print(sys.version)                      # must contain "free-threaded build"
+print(sys.version)
 
 def run(args: Namespace) -> Matrix:
     fd = os.open(args.file, os.O_RDONLY)
-    matrix = load(fd, args.thread)
+    matrix = file.load(fd, args.thread)
     os.close(fd)
     return matrix
 
@@ -26,10 +26,4 @@ if __name__ == '__main__':
     if args.thread == None:
         raise ValueError('thread numbers setted to None')
 
-    if args.b:
-        with timer(label="load") as t:
-            matrix = run(args)
-        print(f"seconds: {t.s}")
-        print(f"milliseconds: {t.ms}")
-    else:
-        run(args)
+    run(args)
