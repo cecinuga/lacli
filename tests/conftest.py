ba1c41547cff3084dbc37f638dc21a4e6f55c5d4
cecinuga <cecinuga.tdb@gmail.com>
2026-06-26 23:56:18 +0200

    support for until  thread

--- tests/conftest.py
+++ tests/conftest.py
@@ -33,5 +33,5 @@ def csv_reader(dataset_path):
     params=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32]
 )
 def n_thread(request):
-    """Yield each candidate worker-thread count to exercise `run` under varying parallelism."""
+    """Yield each candidate worker-thread count to exercise varying levels of parallelism."""
     yield request.param
