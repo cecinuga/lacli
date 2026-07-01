f7b24c7b17edc0b017e7a2a721466d41d500fc71
cecinuga <cecinuga.tdb@gmail.com>
2026-07-01 22:05:15 +0200

    created custom exception

---
+++ src/linacon/models/__init__.py
@@ -0,0 +1,4 @@
+"""Custom exceptions"""
+from linacon.models.exce import ShapeException
+
+__all__ = ["ShapeException"]
---
+++ src/linacon/models/exce.py
@@ -0,0 +1,3 @@
+class ShapeException(Exception):
+    def __init__(self, message):
+        self.message = message
